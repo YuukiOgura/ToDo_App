6b34ac7a068fe90a27f88c54805c49295356d6d0
YuukiOgura <ithamamatsu.ogura@gmail.com>
2023-12-02 16:10:37 +0900

    調整

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -97,20 +97,14 @@
                     </div>
                   </div>
                 </div>
+              </div>
             </form>
           </div>
+        </div>
       </section>
     </div>
   </div>
 @endsection
 @section('bottom_script')
-  <script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
-  <script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
-  <script>
-    flatpickr(document.getElementById('due_date'), {
-      locale: 'ja',
-      dateFormat: "Y/m/d",
-      minDate: new Date()
-    });
-  </script>
+  @include('layouts/todo/flatpickr')
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -19,48 +19,48 @@
                 <div class="flex flex-wrap -m-2">
                   <div class="p-2 w-full">
                     <div class="relative">
-                      <label for="title_edit" class="leading-7 text-sm text-gray-600">
+                      <label for="title" class="leading-7 text-sm text-gray-600">
                         タスク名
                       </label> <br><!--forとidで紐づけ-->
-                      <input type="text" name="title_edit" id="title_edit" value={{ $tasks->title }}
+                      <input type="text" name="title" id="title" value={{ $tasks->title }}
                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                       <!--nameは送信された時のキー名-->
                     </div>
                   </div>
                   <div class="p-2 w-full">
                     <div class="relative">
-                      <label for="textarea_edit" class="leading-7 text-sm text-gray-600">タスク説明文</label> <br>
-                      <textarea name="textarea_edit" id="textarea_edit" cols="50" rows="3"
+                      <label for="textarea" class="leading-7 text-sm text-gray-600">タスク説明文</label> <br>
+                      <textarea name="textarea" id="textarea" cols="50" rows="3"
                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $tasks->textarea }}</textarea><br>
                       {{-- タスクの説明 --}}
                     </div>
                   </div>
                   <div class="p-2 w-full">
                     <div class="relative">
-                      <label for="due_date_edit" class="leading-7 text-sm text-gray-600">
+                      <label for="due_date" class="leading-7 text-sm text-gray-600">
                         期限
                       </label> <br>
-                      <input type="text" name="due_date_edit" id="due_date_edit" value={{ $tasks->due_date }}
+                      <input type="text" name="due_date" id="due_date" value={{ $tasks->due_date }}
                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                     </div>
 
                     <div class="p-2 w-full">
                       <div class="relative">
-                        <label for="priority_edit" class="leading-7 text-sm text-gray-600">重要度を選択してください</label> <br>
+                        <label for="priority" class="leading-7 text-sm text-gray-600">重要度を選択してください</label> <br>
                         <div
                           class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
 
-                          <input type="radio" name ="priority_edit" value = "1"
-                            {{ $tasks->priority == 1 ? 'checked' : '' }} id = "priority_edit1" class="mr-2">
-                          <label for="priority_edit1" class="mr-4">重要</label>
+                          <input type="radio" name ="priority" value = "1"
+                            {{ $tasks->priority == 1 ? 'checked' : '' }} id = "priority1" class="mr-2">
+                          <label for="priority1" class="mr-4">重要</label>
 
-                          <input type="radio" name ="priority_edit" value = "2"
-                            {{ $tasks->priority == 2 ? 'checked' : '' }} id = "priority_edit2" class="mx-4">
-                          <label for="priority_edit2" class="mr-4">普通</label>
+                          <input type="radio" name ="priority" value = "2"
+                            {{ $tasks->priority == 2 ? 'checked' : '' }} id = "priority2" class="mx-4">
+                          <label for="priority2" class="mr-4">普通</label>
 
-                          <input type="radio" name ="priority_edit" value = "3"
-                            {{ $tasks->priority == 3 ? 'checked' : '' }} id = "priority_edit3" class="ml-2">
-                          <label for="priority_edit3">後回し</label>
+                          <input type="radio" name ="priority" value = "3"
+                            {{ $tasks->priority == 3 ? 'checked' : '' }} id = "priority3" class="ml-2">
+                          <label for="priority3">後回し</label>
 
                           {{-- 重要度 --}}
                         </div>
@@ -86,13 +86,5 @@
   </div>
 @endsection
 @section('bottom_script')
-<script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
-<script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
-<script>
-  flatpickr(document.getElementById('due_date_edit'), {
-    locale: 'ja',
-    dateFormat: "Y/m/d",
-    minDate: new Date()
-  });
-</script>
+  @include('layouts/todo/flatpickr')
 @endsection
