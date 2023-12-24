64850f6ba80d5510fe88ead83f61b62ca87c6098
ogurayuuki <ithamamatsu.ogura@gmail.com>
2023-12-24 09:10:00 +0900

    Gmail SMTP連携

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -48,5 +50,11 @@ class User extends Authenticatable
     {
         return $this->hasMany(Folder::class);
     }
+
+    public function sendPasswordResetNotification($token): void
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
+
 
 }
